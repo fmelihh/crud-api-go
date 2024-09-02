70b0edafbdb17b13bcbd9168c62098843427175a
fmelihh <furkanmelihercan.98@gmail.com>
2024-09-02 22:12:44 +0300

    feat: user

--- service/user/routes.go
+++ service/user/routes.go
@@ -3,14 +3,17 @@ package user
 import (
 	"net/http"
 
+	"github.com/fmelihh/crud-api-go/types"
+	"github.com/fmelihh/crud-api-go/utils"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -23,5 +26,9 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 
 }
---
+++ types/types.go
@@ -0,0 +1,25 @@
+package types
+
+import "time"
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:email`
+	Password  string `json:password`
+}
