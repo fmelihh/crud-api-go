c4c8c761291d653f9733b2c5e00b9bf4dd0d4296
fmelihh <furkanmelihercan.98@gmail.com>
2024-09-02 22:22:48 +0300

    feat: register handler

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 )
--- service/user/routes.go
+++ service/user/routes.go
@@ -1,8 +1,10 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/fmelihh/crud-api-go/service/auth"
 	"github.com/fmelihh/crud-api-go/types"
 	"github.com/fmelihh/crud-api-go/utils"
 	"github.com/gorilla/mux"
@@ -31,4 +33,26 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 		utils.WriteError(w, http.StatusBadRequest, err)
 	}
 
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
